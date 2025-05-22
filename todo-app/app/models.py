b32582fedae5e07f8b78483247ccd458ba89285a
Diem Do <ngocdiemdo04@gmail.com>
2025-05-22 15:41:16 +0000

    fix table

--- todo-app/app/models.py
+++ todo-app/app/models.py
@@ -2,6 +2,7 @@ from . import db
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
@@ -11,5 +12,5 @@ class Task(db.Model):
     description = db.Column(db.String(255), nullable=False)
     details = db.Column(db.String(100), nullable=True)
     completed = db.Column(db.Boolean, default=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     user = db.relationship('User', backref=db.backref('tasks', lazy=True))
